00c2079cca8590d17a178b64eb4581175b4f9dea
Mamour DIOP <89989089+Mdev98@users.noreply.github.com>
2024-09-01 19:17:37 +0000

    Update urls.py

--- authentication/urls.py
+++ authentication/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('sign-up/', views.sign_up, name='sign-up'),
-    path('sign-in/', views.sign_in, name='sign-in'),
+    path('', views.sign_in, name='sign-in'),
     path('sign-out/', views.sign_out, name='sign-out'),
     path('profile/', views.profile, name='profile'),
     path('forgot-password/', views.forgot_password, name='forgot-password'),
