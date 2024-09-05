2c7841f5d1016e50247f179654583240a34eec2e
Mamour DIOP <89989089+Mdev98@users.noreply.github.com>
2024-09-05 16:34:31 +0000

    Update settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=a=r-qaib=!l==wv78a$+c24unemqey78#g37)q96r7s@g!vau'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
